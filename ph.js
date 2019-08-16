19dc88aae98575628ed4437e342d0115b4f4266c
jero99 <jero98772@gmail.com>
2019-08-16 14:30:52 -0500

    arreglando errores

--- ph.js
+++ ph.js
@@ -25,12 +25,19 @@ cointain();
 if (selected=='POH') {
 	Kb.disabled = true;
 	Ka.disabled=false;
+	sal.disabled=false;
 	poh()
 }
-else{
+if (selected=='PH') {
 	Ka.disabled = true;
 	Kb.disabled=false;
+	sal.disabled=false;
 	ph()
+}
+if (selected=='henderson_hasselbalch') {
+	Ka.disabled = true;
+	Kb.disabled=false;
+	henderson_hasselbalch(sal,moles,ka)
 }
 }
 
@@ -48,17 +55,17 @@ cointain()
 
 	exponent=parseInt(exponent);
 	console.log(exponent)
-	Kb=parseFloat(Kb);
-	console.log(Kb)
+	Ka=parseFloat(Ka);
+	console.log(Ka)
 	moles=parseFloat(moles);
 	console.log(moles)
 	//divide= Math.pow(Ka,exponent)
-	divide=Kb*10**(exponent)
+	divide=Ka*10**(exponent)
 	//divide= Ka*moles
 	console.log(divide)
 	divide/=moles
 	console.log("divicion",divide)
-	multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Kb)+"*10**"+exponent+"  /  "+String(moles)+"  =  "+String(divide)+"</h2>"
+	multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Ka)+"*10**"+exponent+"  /  "+String(moles)+"  =  "+String(divide)+"</h2>"
 	divide.innerHTML=multiple_out
 
 	sqrt= Math.sqrt(parseFloat(divide));
@@ -79,7 +86,7 @@ cointain()
 		//pH_subtraction_3.innerHTML=pH_subtraction_out
 		//console.log(pH_subtraction)
 		console.log("verificar")
-		multiple=Kb*10**(exponent)
+		multiple=Ka*10**(exponent)
 		multiple/=parseFloat(moles)
 		console.log(divide)
 		sqrt=sqrt= Math.sqrt(parseFloat(divide));
@@ -184,17 +191,17 @@ cointain()
 
 	exponent=parseInt(exponent);
 	console.log(exponent)
-	Ka=parseFloat(Ka);
-	console.log(Ka)
+	Kb=parseFloat(Kb);
+	console.log(Kb)
 	moles=parseFloat(moles);
 	console.log(moles)
 	//divide= Math.pow(Ka,exponent)
-	divide=Ka*10**(exponent)
+	divide=Kb*10**(exponent)
 	//divide= Ka*moles
 	console.log(divide)
 	divide/=moles
 	console.log(divide)
-	multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Ka)+"*10**"+exponent+"  /  "+String(moles)+"  =  "+String(divide)+"</h2>"
+	multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Kb)+"*10**"+exponent+"  /  "+String(moles)+"  =  "+String(divide)+"</h2>"
 	divide.innerHTML=multiple_out
 
 	sqrt= Math.sqrt(parseFloat(divide));
@@ -214,7 +221,7 @@ cointain()
 		pH_subtraction_3.innerHTML=pH_subtraction_out
 		console.log(pH_subtraction)
 		console.log("verificar")
-		multiple=Ka*10**(exponent)
+		multiple=Kb*10**(exponent)
 		multiple/=parseFloat(moles)
 		console.log(divide)
 		sqrt=sqrt= Math.sqrt(parseFloat(divide));
@@ -228,10 +235,10 @@ cointain()
 		verificar.innerHTML="posble error de calculo =( ";
 		if (bool) {
 			console.log("paso",pH_subtraction2)
-			multiple=Ka*10**(exponent)
+			multiple=Kb*10**(exponent)
 			multiple*=moles
 			console.log(multiple)
-			multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Ka)+"*10**"+exponent+"  *  "+String(moles)+"  =  "+String(multiple)+"</h2>"
+			multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Kb)+"*10**"+exponent+"  *  "+String(moles)+"  =  "+String(multiple)+"</h2>"
 			multiple_0.innerHTML=multiple_out
 			sqrt=Math.sqrt(multiple);
 			sqrt_out="<h2>"+" saca  raiz cudrada para hallar X1 OH- o H+ :</br>"+"sqrt(  "+String(multiple)+"  )=  "+String(sqrt)+"</h2>"
@@ -260,7 +267,7 @@ cointain()
 		pH_subtraction_3.innerHTML=pH_subtraction_out
 		console.log(pH_subtraction)
 		console.log("verificar")
-		multiple=Ka*10**(exponent)
+		multiple=Kb*10**(exponent)
 		multiple/=parseFloat(moles)
 		console.log(divide)
 		sqrt=sqrt= Math.sqrt(parseFloat(divide));
@@ -273,10 +280,10 @@ cointain()
 		console.log(bool)
 		if (bool) {
 			console.log("paso",pH_subtraction2)
-			multiple=Ka*10**(exponent)
+			multiple=Kb*10**(exponent)
 			multiple*=moles
 			console.log(multiple)
-			multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Ka)+"*10**"+exponent+"  *  "+String(moles)+"  =  "+String(multiple)+"</h2>"
+			multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Kb)+"*10**"+exponent+"  *  "+String(moles)+"  =  "+String(multiple)+"</h2>"
 			multiple_0.innerHTML=multiple_out
 			sqrt=Math.sqrt(multiple);
 			sqrt_out="<h2>"+" saca  raiz cudrada para hallar X1 OH- o H+ :</br>"+"sqrt(  "+String(multiple)+"  )=  "+String(sqrt)+"</h2>"
@@ -299,4 +306,29 @@ cointain()
 }
 function h(){
 cointain()
+}
+function  henderson_hasselbalch(sal,mol,ka){//sal,Moles,kb_o_ka
+	cointain();
+
+	acido="<h1> y es un acido</h1>"
+	acid_or_base.innerHTML=acido;
+	step_0.innerHTML="paso 0 multiplicacion";
+	step_1.innerHTML="paso 1 raiz cuadrada";
+	step_2.innerHTML="paso 2 logaritmo";
+	step_3.innerHTML="paso 3 subtraccion a PH";
+	step_4.innerHTML="paso 4 acido o base";
+	step_5.innerHTML="paso 5 verificar";
+
+	exponent=parseInt(exponent);
+	console.log(exponent)
+	Ka=parseFloat(Ka);
+	console.log(Ka)
+	moles=parseFloat(moles);
+	console.log(moles)
+	Ka=Ka*10**(exponent)
+	console.log(ka)
+	henderson_hasselbalch=Math.log10(sal)
+	console.log("divicion",divide)
+//	multiple_out="<h2>"+"multiplicacion para hallar X2 que es OH- o H+ :</br>"+String(Kb)+"*10**"+exponent+"  /  "+String(moles)+"  =  "+String(divide)+"</h2>"
+// 	multiple_0.innerHTML=multiple_out
 }
